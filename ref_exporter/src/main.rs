70c35da900bb891c49a7c3d316d03aea2de4363e
Cassie Grey <cassgrey05@gmail.com>
2025-07-21 16:34:27 -0700

    adding ref scene stuff

--- ref_exporter/src/main.rs
+++ ref_exporter/src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use iso::{IsoPlugin, IsoCamera, RefCamera, RefCharacter};
 use debug::DebugPlugin;
+use bevy_egui::{egui, EguiContexts};
 
 fn main() {
     let mut app = App::new();
@@ -10,6 +11,7 @@ fn main() {
     app.add_plugins(DebugPlugin);
 
     app.add_systems(Startup, startup);
+    app.add_systems(Update, ui);
 
     app.run();
 }
@@ -18,4 +20,12 @@ fn startup(mut commands: Commands) {
     commands.spawn(RefCamera);
     commands.spawn(RefCharacter);
     commands.spawn((PointLight::default(), Transform::from_xyz(3.0, 8.0, 5.0)));
+}
+
+fn ui(mut egui_contexts: EguiContexts) {
+    if let Ok(ctx) = egui_contexts.ctx_mut() {
+        egui::Window::new("Title").show(egui_contexts.ctx_mut().unwrap(), |ui| {
+            ui.label("hello!");
+        });
+    }
 }
